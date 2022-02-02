d4c9de7f0a5f570230ce3759a378f70a0b918d6e
olexandria <olexandra2001>
2022-02-02 14:12:48 +0200

    Pre-commit-hook

--- cookBook/wsgi.py
+++ cookBook/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cookBook.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cookBook.settings")
 
 application = get_wsgi_application()
