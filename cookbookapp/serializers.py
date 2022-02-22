57c27beb1e99225b367cd7f28c7bced5833f7a71
olexandria <olexandra2001>
2022-02-22 21:03:18 +0200

    Create CRUD operations

--- cookbookapp/serializers.py
+++ cookbookapp/serializers.py
@@ -5,7 +5,7 @@ from .models import Recipe, Ingredient
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
-        fields = ("name", "amount")
+        fields = ("id", "name", "amount")
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -13,4 +13,28 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ("name", "description", "steps", "image", "ingredients")
+        fields = ("id", "name", "description", "steps", "image", "ingredients")
+
+    def create(self, validated_data):
+        ingredients_data = validated_data.pop("ingredients")
+        recipe = Recipe.objects.create(**validated_data)
+        for ingredient_data in ingredients_data:
+            Ingredient.objects.create(recipe=recipe, **ingredient_data)
+        return recipe
+
+    def update(self, instance, validated_data):
+        ingredients_data = validated_data.pop("ingredients")
+        ingredients = (instance.ingredients).all()
+        ingredients = list(ingredients)
+        instance.name = validated_data.get("name", instance.name)
+        instance.description = validated_data.get("description", instance.description)
+        instance.steps = validated_data.get("steps", instance.steps)
+        instance.image = validated_data.get("image", instance.image)
+        instance.save()
+
+        for ingredient_data in ingredients_data:
+            ingredient = ingredients.pop(0)
+            ingredient.name = ingredient_data.get("name", ingredient.name)
+            ingredient.amount = ingredient_data.get("amount", ingredient.amount)
+            ingredient.save()
+        return instance
--- cookbookapp/views.py
+++ cookbookapp/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+
 from .models import Recipe, Ingredient
 from .serializers import RecipeSerializer, IngredientSerializer
 
@@ -6,6 +9,42 @@ from .serializers import RecipeSerializer, IngredientSerializer
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all().order_by("name")
     serializer_class = RecipeSerializer
+
+    def get(request, pk=None):
+        if pk:
+            recipe = get_object_or_404(Recipe.objects.all(), pk=pk)
+            serializer = RecipeSerializer(recipe)
+            return Response({"recipe": serializer.data})
+        recipe = Recipe.objects.all()
+        serializer = RecipeSerializer(recipe, many=True)
+        return Response({"recipes": serializer.data})
+
+    def post(self, request):
+        recipe = request.data.get("recipe")
+        serializer = RecipeSerializer(data=recipe)
+        if serializer.is_valid(raise_exception=True):
+            recipe_saved = serializer.save()
+        return Response(
+            {"success": "Recipe '{}' created successfully".format(recipe_saved.title)}
+        )
+
+    def put(self, request, pk):
+        saved_recipe = get_object_or_404(Recipe.objects.all(), pk=pk)
+        data = request.data.get("recipe")
+        serializer = RecipeSerializer(instance=saved_recipe, data=data, partial=True)
+
+        if serializer.is_valid(raise_exception=True):
+            recipe_saved = serializer.save()
+        return Response(
+            {"success": "Recipe '{}' updated successfully".format(recipe_saved.title)}
+        )
+
+    def delete(self, request, pk):
+        recipe = get_object_or_404(Recipe.objects.all(), pk=pk)
+        recipe.delete()
+        return Response(
+            {"message": "Recipe with id `{}` has been deleted.".format(pk)}, status=204
+        )
 
 
 class IngredientViewSet(viewsets.ModelViewSet):
