cc37a157e5b98c05d8b32e50aa61b4bd6a03bc14
olexandria <olexandra2001>
2022-03-03 17:15:44 +0200

    Added urls

--- cookbookapp/urls.py
+++ cookbookapp/urls.py
@@ -1,8 +1,29 @@
-from rest_framework import routers
-from .views import RecipeViewSet, IngredientViewSet
+from django.urls import path
+from . import views
 
-router = routers.DefaultRouter()
-router.register(r"recipes", RecipeViewSet)
-router.register(r"ingredients", IngredientViewSet)
-
-urlpatterns = router.urls
+urlpatterns = [
+    path("", views.RecipeViewSet.as_view({"get": "list"}), name="recipes"),
+    path(
+        "<int:pk>/",
+        views.RecipeViewSet.as_view({"get": "retrieve"}),
+        name="recipe-detail",
+    ),
+    path(
+        "create", views.RecipeViewSet.as_view({"post": "create"}), name="recipe-create"
+    ),
+    path(
+        "<int:pk>/update",
+        views.RecipeViewSet.as_view({"get": "retrieve", "put": "update"}),
+        name="recipe-update",
+    ),
+    path(
+        "<int:pk>/delete",
+        views.RecipeViewSet.as_view({"get": "retrieve", "delete": "destroy"}),
+        name="recipe-delete",
+    ),
+    path(
+        "<int:pk>/delete",
+        views.RecipeViewSet.as_view({"get": "retrieve", "delete": "destroy"}),
+        name="recipe-delete",
+    ),
+]
