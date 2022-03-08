1698c80984b4f52d470ae79786ca2f6f99775169
olexandria <olexandra2001>
2022-03-08 20:27:53 +0200

    Update serializers

--- cookbookapp/serializers.py
+++ cookbookapp/serializers.py
@@ -16,6 +16,13 @@ class RecipeSerializer(WritableNestedModelSerializer):
         model = Recipe
         fields = ("id", "name", "description", "steps", "image", "ingredients")
 
+    # def create(self, validated_data):
+    #     ingredients_data = validated_data.pop("ingredients")
+    #     recipe = Recipe.objects.create(**validated_data)
+    #     for ingredient_data in ingredients_data:
+    #         Ingredient.objects.create(recipe=recipe, **ingredient_data)
+    #     return recipe
+    #
     # def update(self, instance, validated_data):
     #     ingredients_data = validated_data.pop("ingredients")
     #     ingredients = (instance.ingredients).all()
